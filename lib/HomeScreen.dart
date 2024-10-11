cc253f084ab42a6fb1ac481de38d3ac6b7e9dba4
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-10-11 09:14:53 -0700

    remove advanced notifications

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,21 +1,9 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 
-// ignore: unnecessary_import
-import 'dart:typed_data';
-
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-import 'package:timezone/data/latest_all.dart' as tz;
-import 'package:timezone/timezone.dart' as tz;
-import 'package:timezone/timezone.dart';
-import 'package:http/http.dart' as http;
-
 
 
 class HomeScreen extends StatefulWidget {
@@ -29,7 +17,6 @@ class _HomeScreenState extends State<HomeScreen> {
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
   int number = 1;
-  bool _notificationsEnabled = false;
 
   @override
   void initState() {
@@ -59,7 +46,6 @@ class _HomeScreenState extends State<HomeScreen> {
           false;
 
       setState(() {
-        _notificationsEnabled = granted;
       });
     }
   }
@@ -90,7 +76,6 @@ class _HomeScreenState extends State<HomeScreen> {
       final bool? grantedNotificationPermission =
           await androidImplementation?.requestNotificationsPermission();
       setState(() {
-        _notificationsEnabled = grantedNotificationPermission ?? false;
       });
     }
   }
@@ -109,284 +94,6 @@ class _HomeScreenState extends State<HomeScreen> {
         payload: 'item x');
   }
 
-  Future<void> _showNotificationWithActions() async {
-    const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails(
-      'your channel id',
-      'your channel name',
-      channelDescription: 'your channel description',
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker',
-      actions: <AndroidNotificationAction>[
-        AndroidNotificationAction(
-          "id_1",
-          'Action 1',
-          icon: DrawableResourceAndroidBitmap('mipmap/ic_launcher'),
-          contextual: true,
-        ),
-        AndroidNotificationAction(
-          'id_2',
-          'Action 2',
-          titleColor: Color.fromARGB(255, 255, 0, 0),
-          icon: DrawableResourceAndroidBitmap('mipmap/ic_launcher'),
-        ),
-        AndroidNotificationAction(
-          'id_3',
-          'Action 3',
-          icon: DrawableResourceAndroidBitmap('mipmap/ic_launcher'),
-          showsUserInterface: true,
-          // By default, Android plugin will dismiss the notification when the
-          // user tapped on a action (this mimics the behavior on iOS).
-          cancelNotification: false,
-        ),
-      ],
-    );
-
-    const NotificationDetails notificationDetails = NotificationDetails(
-      android: androidNotificationDetails,
-    );
-    await flutterLocalNotificationsPlugin.show(
-        number, 'plain title', 'plain body', notificationDetails,
-        payload: 'item z');
-  }
-
-  Future<void> _showNotificationWithTextAction() async {
-    const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails(
-      'your channel id',
-      'your channel name',
-      channelDescription: 'your channel description',
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker',
-      actions: <AndroidNotificationAction>[
-        AndroidNotificationAction(
-          'text_id_1',
-          'Enter Text',
-          icon: DrawableResourceAndroidBitmap('mipmap/ic_launcher'),
-          inputs: <AndroidNotificationActionInput>[
-            AndroidNotificationActionInput(
-              label: 'Enter a message',
-            ),
-          ],
-        ),
-      ],
-    );
-
-    const NotificationDetails notificationDetails = NotificationDetails(
-      android: androidNotificationDetails,
-    );
-
-    await flutterLocalNotificationsPlugin.show(
-        number,
-        'Text Input Notification',
-        'Expand to see input action',
-        notificationDetails,
-        payload: 'item x');
-  }
-
-  Future<void> _showNotificationWithTextChoice() async {
-    const AndroidNotificationDetails androidNotificationDetails =
-        AndroidNotificationDetails(
-      'your channel id',
-      'your channel name',
-      channelDescription: 'your channel description',
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker',
-      actions: <AndroidNotificationAction>[
-        AndroidNotificationAction(
-          'text_id_2',
-          'Action 2',
-          icon: DrawableResourceAndroidBitmap('mipmap/ic_launcher'),
-          inputs: <AndroidNotificationActionInput>[
-            AndroidNotificationActionInput(
-              choices: <String>['ABC', 'DEF'],
-              allowFreeFormInput: false,
-            ),
-          ],
-          contextual: true,
-        ),
-      ],
-    );
-
-    const NotificationDetails notificationDetails = NotificationDetails(
-      android: androidNotificationDetails,
-    );
-    await flutterLocalNotificationsPlugin.show(
-        number, 'plain title', 'plain body', notificationDetails,
-        payload: 'item x');
-  }
-
-  Future<void> _showFullScreenNotification() async {
-    await showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: const Text('Turn off your screen'),
-        content: const Text(
-            'to see the full-screen intent in 5 seconds, press OK and TURN '
-            'OFF your screen'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Cancel'),
-          ),
-          TextButton(
-            onPressed: () async {
-              await flutterLocalNotificationsPlugin.zonedSchedule(
-                  0,
-                  'scheduled title',
-                  'scheduled body',
-                  TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
-                  const NotificationDetails(
-                      android: AndroidNotificationDetails(
-                          'full screen channel id', 'full screen channel name',
-                          channelDescription: 'full screen channel description',
-                          priority: Priority.high,
-                          importance: Importance.high,
-                          fullScreenIntent: true)),
-                  androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-                  uiLocalNotificationDateInterpretation:
-                      UILocalNotificationDateInterpretation.absoluteTime);
-
-              Navigator.pop(context);
-            },
-            child: const Text('OK'),
-          )
-        ],
-      ),
-    );
-  }
-
-  Future<String> _base64encodedImage(String url) async {
-    final http.Response response = await http.get(Uri.parse(url));
-    final String base64Data = base64Encode(response.bodyBytes);
-    return base64Data;
-  }
-
-  Future<void> _showBigPictureNotificationBase64() async {
-    final String largeIcon =
-    await _base64encodedImage('https://dummyimage.com/48x48');
-    final String bigPicture =
-    await _base64encodedImage('https://dummyimage.com/400x800');
-
-    final BigPictureStyleInformation bigPictureStyleInformation =
-    BigPictureStyleInformation(
-        ByteArrayAndroidBitmap.fromBase64String(
-            bigPicture), //Base64AndroidBitmap(bigPicture),
-        largeIcon: ByteArrayAndroidBitmap.fromBase64String(largeIcon),
-        contentTitle: 'overridden <b>big</b> content title',
-        htmlFormatContentTitle: true,
-        summaryText: 'summary <i>text</i>',
-        htmlFormatSummaryText: true);
-    final AndroidNotificationDetails androidNotificationDetails =
-    AndroidNotificationDetails(
-        'big text channel id', 'big text channel name',
-        channelDescription: 'big text channel description',
-        styleInformation: bigPictureStyleInformation);
-    final NotificationDetails notificationDetails =
-    NotificationDetails(android: androidNotificationDetails);
-    await flutterLocalNotificationsPlugin.show(
-        number, 'big text title', 'silent body', notificationDetails);
-  }
-
-  Future<void> _showBigTextNotification() async {
-    const BigTextStyleInformation bigTextStyleInformation =
-    BigTextStyleInformation(
-      'Lorem <i>ipsum dolor sit</i> amet, consectetur <b>adipiscing elit</b>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      htmlFormatBigText: true,
-      contentTitle: 'overridden <b>big</b> content title',
-      htmlFormatContentTitle: true,
-      summaryText: 'summary <i>text</i>',
-      htmlFormatSummaryText: true,
-    );
-    const AndroidNotificationDetails androidNotificationDetails =
-    AndroidNotificationDetails(
-        'big text channel id', 'big text channel name',
-        channelDescription: 'big text channel description',
-        styleInformation: bigTextStyleInformation);
-    const NotificationDetails notificationDetails =
-    NotificationDetails(android: androidNotificationDetails);
-    await flutterLocalNotificationsPlugin.show(
-        number, 'big text title', 'silent body', notificationDetails);
-  }
-
-  Future<void> _repeatNotification() async {
-    const AndroidNotificationDetails androidNotificationDetails =
-    AndroidNotificationDetails(
-        'repeating channel id', 'repeating channel name',
-        channelDescription: 'repeating description');
-    const NotificationDetails notificationDetails =
-    NotificationDetails(android: androidNotificationDetails);
-    await flutterLocalNotificationsPlugin.periodicallyShow(
-      number,
-      'repeating title',
-      'repeating body',
-      RepeatInterval.everyMinute,
-      notificationDetails,
-      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-    );
-  }
-
-  Future<void> _scheduleDailyTenAMNotification() async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-        0,
-        'daily scheduled notification title',
-        'daily scheduled notification body',
-        _nextInstanceOfTenAM(),
-        const NotificationDetails(
-          android: AndroidNotificationDetails('daily notification channel id',
-              'daily notification channel name',
-              channelDescription: 'daily notification description'),
-        ),
-        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-        uiLocalNotificationDateInterpretation:
-        UILocalNotificationDateInterpretation.absoluteTime,
-        matchDateTimeComponents: DateTimeComponents.time);
-  }
-
-  Future<void> _scheduleWeeklyMondayTenAMNotification() async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-        0,
-        'weekly scheduled notification title',
-        'weekly scheduled notification body',
-        _nextInstanceOfMondayTenAM(),
-        const NotificationDetails(
-          android: AndroidNotificationDetails('weekly notification channel id',
-              'weekly notification channel name',
-              channelDescription: 'weekly notificationdescription'),
-        ),
-        androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-        uiLocalNotificationDateInterpretation:
-        UILocalNotificationDateInterpretation.absoluteTime,
-        matchDateTimeComponents: DateTimeComponents.dayOfWeekAndTime);
-  }
-
-  tz.TZDateTime _nextInstanceOfMondayTenAM() {
-    tz.TZDateTime scheduledDate = _nextInstanceOfTenAM();
-    while (scheduledDate.weekday != DateTime.monday) {
-      scheduledDate = scheduledDate.add(const Duration(days: 1));
-    }
-    return scheduledDate;
-  }
-
-  tz.TZDateTime _nextInstanceOfTenAM() {
-    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
-    tz.TZDateTime scheduledDate =
-    tz.TZDateTime(tz.local, now.year, now.month, now.day, 23);
-    if (scheduledDate.isBefore(now)) {
-      scheduledDate = scheduledDate.add(const Duration(days: 1));
-    }
-    return scheduledDate;
-  }
-
-  Future<void> _cancelAllNotifications() async {
-    await flutterLocalNotificationsPlugin.cancelAll();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -402,93 +109,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 number++;
               },
               child: const Text("Simple Notification"),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _showNotificationWithActions();
-                number++;
-              },
-              child: const Text("Action Notification"),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _showNotificationWithTextAction();
-                number++;
-              },
-              child: const Text("Text Notification"),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _showNotificationWithTextChoice();
-                number++;
-              },
-              child: const Text("Choice Notification"),
-            ),
-          ),Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _showFullScreenNotification();
-                number++;
-              },
-              child: const Text("Fullscreen Notification"),
-            ),
-          ),Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _showBigPictureNotificationBase64();
-                number++;
-              },
-              child: const Text("Image Notification"),
-            ),
-          ),Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _showBigTextNotification();
-                number++;
-              },
-              child: const Text("Big Text Notification"),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _repeatNotification();
-                number++;
-              },
-              child: const Text("Every minute notification"),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _scheduleDailyTenAMNotification();
-                number++;
-              },
-              child: const Text('Schedule daily 10:00:00 am notification in your '
-                  'local time zone'),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _scheduleWeeklyMondayTenAMNotification();
-                number++;
-              },
-              child: const Text('Schedule weekly Monday 10:00:00 am notification '
-                  'in your local time zone',),
-            ),
-          ),Center(
-            child: ElevatedButton(
-              onPressed: () {
-                _cancelAllNotifications();
-              },
-              child: const Text('cancel all',),
             ),
           ),
         ],
