eab871aeaea49455287106a69b0be4f3cbc45ef5
jzelAdmin2006 <jo.zeller@protonmail.com>
2024-10-14 20:26:24 -0700

    try out approach that should work on iOS

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'notification_service.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -14,42 +14,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
   int notificationId = 1;
 
   @override
   void initState() {
     super.initState();
-    flutterLocalNotificationsPlugin.initialize(const InitializationSettings(
-        android: AndroidInitializationSettings('mipmap/ic_launcher'), iOS: DarwinInitializationSettings()))
-        .then((_) => _requestPermissions());
-  }
-
-  Future<void> _requestPermissions() async {
-    if (Platform.isIOS) {
-      await flutterLocalNotificationsPlugin
-          .resolvePlatformSpecificImplementation<
-              IOSFlutterLocalNotificationsPlugin>()
-          ?.requestPermissions(
-            alert: true,
-            badge: true,
-            sound: true,
-          );
-    } else if (Platform.isAndroid) {
-          await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
-              AndroidFlutterLocalNotificationsPlugin>()?.requestNotificationsPermission();
-      setState(() {
-      });
-    }
-  }
-
-  Future<void> _showNotification() async {
-    await flutterLocalNotificationsPlugin.show(
-        notificationId++, 'Test Title', 'This is Notification demo', const NotificationDetails(android: AndroidNotificationDetails('SwitchboxNotificationChannel', 'Switchbox Notification Channel',
-        channelDescription: 'Notification Channel for Switchbox',
-        importance: Importance.max,
-        priority: Priority.high)));
+    NotificationService().initializePlatformNotifications();
   }
 
   @override
@@ -63,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
           Center(
             child: ElevatedButton(
               onPressed: () {
-                _showNotification();
+                NotificationService().showNotification();
               },
               child: const Text("Simple Notification"),
             ),
